90b0103444e489af26b60f09d479907e639e7df3
Anton Shapkin <57372391+sh-anton8@users.noreply.github.com>
2022-10-20 10:15:42 +0300

    Save on disk + new example added

--- wikiparser_utils.py
+++ wikiparser_utils.py
@@ -3,13 +3,24 @@ import wikitextparser as wtp
 from tqdm import tqdm
 import pandas as pd
 import re
+import json
+import os
+import string
+
+def del_punkt(str_):
+    return str_.translate(str.maketrans("","", string.punctuation))
+
 
 class WikiRevision:
-    def __init__(self, page_id, revision_id, parent_revision_id, text, comment, title, timestamp):
+    def __init__(self, page_id=None, revision_id=None, parent_revision_id=None, 
+                 text=None, comment=None, title=None, timestamp=None):
         self.text = text
         self.title = title
         self.comment = comment
-        self.timestamp = timestamp
+        try:
+            self.timestamp = timestamp.strftime("%Y/%m/%d %H:%M:%S")
+        except:
+            self.timestamp = ""
         self.page_id = page_id
         self.parent_revision_id = parent_revision_id
         self.revision_id = revision_id
@@ -35,17 +46,48 @@ class WikiRevision:
         tags = self.parsed_text.get_tags()
         for tag in tags:
             title = re.findall(r'title=\s*([^|]+)', tag.string)
-            urls = re.findall(r'url=\s*([^|]+)', tag.string)
+            urls = re.findall(r'url=\s*([^}<|]+)', tag.string)
             
             title_str = title[0] if title else ''
             if urls:
                 for link in urls:
                     links.append((title_str, link.strip()))
-        return links        
+        return links 
+    
+    def save(self, filepath):
+        final_path = f"{filepath}/{self.revision_id}.json"
+        json_obj = {
+            "text": self.text,
+            "title": self.title,
+            "comment": self.comment,
+            "timestamp": self.timestamp,
+            "page_id": self.page_id,
+            "parent_revision_id": self.parent_revision_id,
+            "revision_id": self.revision_id
+        }
+        with open(final_path, 'w', encoding='utf-8') as f:
+            json.dump(json_obj, f)
+        
+    def load(self, filepath):
+        with open(filepath, 'r', encoding='utf-8') as f:
+            json_obj = json.load(f)
+        self.text = json_obj["text"]
+        self.title = json_obj["title"]
+        self.comment = json_obj["comment"]
+        self.timestamp = json_obj["timestamp"]
+        self.page_id = json_obj["page_id"]
+        self.parent_revision_id = json_obj["parent_revision_id"]
+        self.revision_id = json_obj["revision_id"]
+        
+        try:
+            self.parsed_text = wtp.parse(self.text)
+        except:
+            pass
+        
         
 
 class WikiPage:
-    def __init__(self, page_id, title, revisions):
+    def __init__(self, page_id=None, title=None, revisions=None):
         self.page_id = page_id
         self.title = title
         self.revisions = revisions
@@ -58,7 +100,17 @@ class WikiPage:
             new_revision = WikiRevision(self.page_id, r_id, p_id, revision.text, revision.comment, self.title, revision.timestamp)
             revisions[r_id] = new_revision
         return revisions
-
+    
+    def load_revisions(self, filepath):
+        revisions = {}
+        revisions_nums = os.listdir(filepath)
+        revs = sorted([int(part.split('.json')[0]) for part in revisions_nums])
+        for revision in revs:
+            final_path = f"{filepath}/{revision}.json"
+            new_revision = WikiRevision()
+            new_revision.load(final_path)
+            revisions[revision] = new_revision
+        return revisions
 
 class WikiXMLDump:
     def __init__(self, dump_path):
@@ -97,4 +149,23 @@ class WikiXMLDump:
                         id2page2revision[page_id] = {}
                     id2page2revision[page_id][rev_id] = rev
         return id2page, id2page2revision
+    
+    def get_save_on_disk(self, filepath):
+        if not os.path.exists(filepath):
+            os.makedirs(filepath)
+        dump = mwxml.Dump.from_file(open(self.dump_path, encoding="utf-8"))
+        for p in tqdm(dump, position=0, leave=True):
+            revisions = []
+            for rev in p:
+                revisions.append(rev)
+
+            new_page = WikiPage(p.id, p.title, revisions)
+            for revision in revisions:
+                p_id = -1 if revision.parent_id is None else revision.parent_id
+                r_id = revision.id
+                new_revision = WikiRevision(new_page.page_id, r_id, p_id, revision.text, revision.comment, new_page.title, revision.timestamp)
+                final_path = f"{filepath}/{del_punkt(new_page.title)}_{new_page.page_id}"
+                if not os.path.exists(final_path):
+                    os.makedirs(final_path)
+                new_revision.save(f"{final_path}")
             
